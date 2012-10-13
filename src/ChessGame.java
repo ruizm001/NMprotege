e361092ee0e9102ab6ae4a7692645c9a672bc4e8
Michael ruiz <ruizm001@gmail.com>
2012-10-12 20:51:36 -0400

    This is the initial file for Maguire ChessGame

---
+++ src/ChessGame.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author mruiz
+ *
+ */
+public class ChessGame {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
